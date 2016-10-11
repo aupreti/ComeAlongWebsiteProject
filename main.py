b6cba23edc67ad8298edccd003c15b0ab2e2e03e
julieannaniu <julieanna.niu@gmail.com>
2016-10-11 11:10:45 +0800

    updated main

--- main.py
+++ main.py
@@ -38,6 +38,60 @@ aboutTemplate = JINJA_ENVIRONMENT.get_template('templates/about.html')
 contactTemplate = JINJA_ENVIRONMENT.get_template('templates/contact.html')
 supportTemplate = JINJA_ENVIRONMENT.get_template('templates/support.html')
 termsOfServiceTemplate = JINJA_ENVIRONMENT.get_template('templates/terms-of-service.html')
+dashboardTemplate = JINJA_ENVIRONMENT.get_template('templates/dashboard.html')
+panelTest = JINJA_ENVIRONMENT.get_template('templates/sample.html')
+newEventTemplate = JINJA_ENVIRONMENT.get_template('templates/new_event.html')
+contactsTemplate = JINJA_ENVIRONMENT.get_template('templates/contacts.html')
+
+
+
+
+Events = [
+    {'event_title': 'Tyler With the Lab team?',
+    'event_host': 'aupreti',
+    'votes': 5,
+    'response_rate': 75,
+    'top_response': 'No',
+    'top_response_votes': 2,
+    'description': 'Anim pariatur cliche reprehenderit, \
+    enim eiusmod high life accusamus terry richardson \
+    ad squid. 3 wolf moon officia aute, non cupidatat \
+    skateboard dolor brunch. Food truck quinoa nesciunt \
+    laborum eiusmod. Brunch 3 wolf',
+    'accordion': 1,
+    'no': 2,
+    'yes': 4},
+
+    {'event_title': 'Holyoke Mall at 10pm?',
+    'event_host': 'aong',
+    'description': 'Anim pariatur cliche reprehenderit, \
+    enim eiusmod high life accusamus terry richardson \
+    ad squid. 3 wolf moon officia aute, non cupidatat \
+    skateboard dolor brunch. Food truck quinoa nesciunt \
+    laborum eiusmod. Brunch 3 wolf',
+    'response_rate': 65,
+    'top_response': 'Yes',
+    'top_response_votes': 10,
+    'votes': 4,
+    'accordion': 2,
+    'no': 2,
+    'yes': 4},
+
+    {'event_title': 'Where do you all want to meet for dinner?',
+    'event_host': 'jniu',
+    'description': 'Anim pariatur cliche reprehenderit, \
+    enim eiusmod high life accusamus terry richardson \
+    ad squid. 3 wolf moon officia aute, non cupidatat \
+    skateboard dolor brunch. Food truck quinoa nesciunt \
+    laborum eiusmod. Brunch 3 wolf',
+    'response_rate': 55,
+    'top_response': 'Chapin',
+    'top_response_votes': 4,
+    'votes': 3,
+    'accordion': 3,
+    'no': 2,
+    'yes': 4},
+]
 
 
 class SendHandler(webapp2.RequestHandler):
@@ -79,11 +133,34 @@ class TermsOfServiceHandler(webapp2.RequestHandler):
         self.response.write(termsOfServiceTemplate.render())
         self.response.write(footer_template.render())
 
+class DashboardHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(dashboardTemplate.render({'Events': Events}))
+
+class PanelHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(panelTest.render({'Events': Events}))
+
+class NewEventHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(newEventTemplate.render())
+
+class ContactsHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(contactsTemplate.render())
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/send', SendHandler),
     ('/about.html', AboutHandler),
     ('/contact.html', ContactHandler),
     ('/support.html', SupportHandler),
-    ('/terms-of-service.html', TermsOfServiceHandler)
+    ('/terms-of-service.html', TermsOfServiceHandler),
+    ('/dashboard.html', DashboardHandler),
+    ('/sample.html', PanelHandler),
+    ('/new_event.html', NewEventHandler),
+    ('/contacts.html', ContactsHandler)
+
 ], debug=True)
