0bd51869f2eb5e49e46fbecd4f604b10a592a514
Audrey Ong <aongyw@gmail.com>
2016-10-14 18:03:43 +0800

    from urls, template links have been updated to reflect these changes.

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ from google.appengine.api import users
 import json
 import datetime
 
+###################### Backend Specific libraries ##############################
+import oauth2client
+from oauth2client.contrib.appengine import CredentialsNDBProperty
+from oauth2client.contrib.appengine import StorageByKeyName
+from oauth2client import client
+import httplib2
+import jwt
+import random
+import string
+import ast
+from apiclient.discovery import build
+################################################################################
+
 
 JINJA_ENVIRONMENT = jinja2.Environment (
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -44,6 +57,9 @@ newEventTemplate = JINJA_ENVIRONMENT.get_template('templates/new_event.html')
 contactsTemplate = JINJA_ENVIRONMENT.get_template('templates/contacts.html')
 
 
+#### temp template. Please make new 404(?) error page###########################
+hiHacker_template = JINJA_ENVIRONMENT.get_template('templates/hiHacker.html')
+#################################################################################
 
 
 Events = [
@@ -100,10 +116,19 @@ class SendHandler(webapp2.RequestHandler):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(header_template.render())
-        self.response.write(navbarTemplate.render())
-        self.response.write(bodyTemplate.render())
-        self.response.write(footer_template.render())
+        # this may return a continous loop between MainHandler and GoogleLoginHandler if not careful
+        # as long as getSession(self) wipes the jwt everytime it does not return True, it should be fine.
+
+        encodedJWT = self.request.cookies.get("session")
+        if encodedJWT is None:
+            self.response.write(header_template.render())
+            self.response.write(navbarTemplate.render())
+            self.response.write(bodyTemplate.render())
+            self.response.write(footer_template.render())
+
+        elif getSession(self):
+            self.redirect('/dashboard')
+
 
 class AboutHandler(webapp2.RequestHandler):
     def get(self):
@@ -135,7 +160,9 @@ class TermsOfServiceHandler(webapp2.RequestHandler):
 
 class DashboardHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(dashboardTemplate.render({'Events': Events}))
+        # anything that requires login do "if getSession(self):"
+        if getSession(self):
+            self.response.write(dashboardTemplate.render({'Events': Events}))
 
 class PanelHandler(webapp2.RequestHandler):
     def get(self):
@@ -143,24 +170,174 @@ class PanelHandler(webapp2.RequestHandler):
 
 class NewEventHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(newEventTemplate.render())
+        if getSession(self):
+            self.response.write(newEventTemplate.render())
 
 class ContactsHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(contactsTemplate.render())
+        if getSession(self):
+            self.response.write(contactsTemplate.render())
 
+
+############################## Backend Code ###################################
+
+flow = client.flow_from_clientsecrets(
+    'notpublicallyaccessible/client_secrets.json',
+    scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/contacts.readonly',
+    redirect_uri='http://localhost:10080/googlelogin')
+
+
+class CAUser(ndb.Model):
+    userID = ndb.IntegerProperty()
+    username = ndb.StringProperty()
+    gmail = ndb.StringProperty()
+    facebook = ndb.StringProperty()
+    credentials = CredentialsNDBProperty()
+    secret = ndb.StringProperty()
+
+
+class EmptySecret(Exception):
+   pass
+
+
+def getSession(self):
+    # returns True if user is logged in, and returns False if user is not logged in, or jwt is tampered with.
+
+    # ways login check can fail:
+    # 1) There is no jwt stored;
+    # 2) The jwt is nonsense (e.g. aaa.bbb.ccc), jwt.decode() will throw an error -> need to handle that (this also emcompases other jwt errors like a jwt that is "asdf" i.e. only one segment long)
+    # 3) The jwt is modified so it doesn't contain the claim "sub".
+    # 4) THe jwt is modified so that "sub"/user does not exist in the database
+    # 5) The jwt is modified so that while the user does exist in the database the secret does not match.
+    # 6) What if no secret is stored in the database?  ---> It actually passes. So when we log the user out, we never delete the secret in the database, but when the user logs back in, we just rewrite over the exisitng secret. This is important!! Or someone could just create and stored a jwt with the secret being empty, and it will be able to pass our checks. Other things to keep note of then, with only one secret being stored, and the secret being reissued each time, a user can't be logged in to more than one device. The only way is if the secret is permanent for each user, so when the user logs in, we merely retrieve the secret and reissue the jwt.  --> maybe put in an extra safe security check in the future to check if the secret in database is empty
+
+    # need to consider in the future if malicious code will be run when reading the uncheckedPayload if code was stored in the jwt -> maybe have some sort of try and except?
+
+    encodedJWT = self.request.cookies.get("session")
+
+    if encodedJWT is None:
+        # is no jwt is stored, redirect user to login page
+        logging.info("No jwt found.")
+        self.redirect("/")
+        return False
+
+    try:
+        uncheckedPayload = jwt.decode(encodedJWT, verify=False)
+        userClaimsToBe = uncheckedPayload["sub"]
+        secretToTestUserClaim = CAUser.query(CAUser.username == userClaimsToBe).get().secret
+
+        if secretToTestUserClaim == "":
+            raise EmptySecret
+
+        try:
+            jwt.decode(encodedJWT, secretToTestUserClaim, algorithms=["HS256"])
+
+        except jwt.DecodeError as e:
+            logging.info("Verification of jwt failed because secret in jwt did not match secret in database (see no. 5).")
+            logging.info(e)
+            self.response.delete_cookie("session")
+            self.response.write(hiHacker_template.render())
+            return False
+
+        else:
+            # Yay verification has passed! User identified and logged in.
+            return True
+
+    except jwt.DecodeError as e:
+        logging.info("Invalid jwt. Jwt decoder had trouble decoding jwt. Examples of jwts that could cause this error is aaa.bbb.ccc (see no. 2 in list). See specific error message by jwt library below:")
+        logging.info(e) # for some reason this logs things twice
+        self.response.delete_cookie("session")
+        self.response.write(hiHacker_template.render())
+        return False
+
+    except KeyError:
+        logging.info("The sub claim was not found in the payload. Invalid jwt. (See no. 3)")
+        self.response.delete_cookie("session")
+        self.response.write(hiHacker_template.render())
+        return False
+
+    except AttributeError:
+        # CAUser.query(CAUser.username == userClaimsToBe).get() returns a None object if the username does not exist in the database. Since it is a None object, secret raises an AttributeError.
+        logging.info("User contained in jwt was not found in database. Invalid jwt. (See no. 4)")
+        self.response.delete_cookie("session")
+        self.response.write(hiHacker_template.render())
+        return False
+
+    except EmptySecret:
+        logging.info("secret for user is empty (see no. 6). The user being:")
+        logging.info(userClaimsToBe)
+        self.response.delete_cookie("session")
+        self.response.write(hiHacker_template.render())
+        return False
+
+    except Exception as e:
+        logging.info("Unexpected jwt problem occured. See below:")
+        logging.info(e)
+        self.response.delete_cookie("session")
+        self.response.write(hiHacker_template.render())
+        return False
+
+    logging.info("getSession shouldn't reach here. Critical error detected. Send help.")
+    self.response.delete_cookie("session")
+    return False
+
+
+
+class GoogleLoginHandler(webapp2.RequestHandler):
+    def get(self):
+        code = self.request.get('code')
+
+        if code == "":
+            auth_uri = flow.step1_get_authorize_url()
+            self.redirect(str(auth_uri))
+
+        else:
+            credentialsToStore = flow.step2_exchange(code)
+            credentials = credentialsToStore
+
+            checkUserExists = CAUser.query(CAUser.username == 'audrey').get()
+
+            if not checkUserExists:
+                secretToStore = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(30))
+                newUser = CAUser(userID=1, username='audrey', gmail='implementlater?', facebook='NA', credentials = credentialsToStore, secret = secretToStore)
+                newUser.put()
+                encodedJWT = jwt.encode({'sub': 'audrey'}, secretToStore, algorithm='HS256')
+                encodedJWT_String = encodedJWT.decode('utf-8')
+                self.response.set_cookie("session", encodedJWT_String)
+
+            else:
+                encodedJWT = jwt.encode({'sub': 'audrey'}, checkUserExists.secret, algorithm='HS256')
+                encodedJWT_String = encodedJWT.decode('utf-8')
+                self.response.set_cookie("session", encodedJWT_String)
+
+            self.redirect('/dashboard')
+
+
+class FacebookLoginHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+
+class LogoutHandler(webapp2.RequestHandler):
+    def get(self):
+        # given error/issue number 6 for the login jwt considerations, I suppose this is all the line of code we need for logging out.
+        self.response.delete_cookie('session')
+        self.redirect("/")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/send', SendHandler),
-    ('/about.html', AboutHandler),
-    ('/contact.html', ContactHandler),
-    ('/support.html', SupportHandler),
-    ('/terms-of-service.html', TermsOfServiceHandler),
-    ('/dashboard.html', DashboardHandler),
-    ('/sample.html', PanelHandler),
-    ('/new_event.html', NewEventHandler),
-    ('/contacts.html', ContactsHandler)
+    ('/about', AboutHandler),
+    ('/contact', ContactHandler),
+    ('/support', SupportHandler),
+    ('/terms-of-service', TermsOfServiceHandler),
+    ('/dashboard', DashboardHandler),
+    ('/sample', PanelHandler),
+    ('/new_event', NewEventHandler),
+    ('/contacts', ContactsHandler),
+    ('/googlelogin', GoogleLoginHandler),
+    ('/facebooklogin', FacebookLoginHandler),
+    ('/logout', LogoutHandler)
 
 ], debug=True)
